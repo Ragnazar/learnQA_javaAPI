b854afe6e4a457f82b87f5bb0a52eb090ee0c391
Ivan Avdeyev <97154352+Ragnazar@users.noreply.github.com>
2023-07-03 20:30:34 +0300

    Ex11: Тест запроса на метод cookie

--- src/test/java/module_3/CookieTest.java
+++ src/test/java/module_3/CookieTest.java
@@ -4,28 +4,28 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CookieTest {
 
     @Test
     public void cookieTest() {
 
-        Response saveExpected = RestAssured
-                .get("https://playground.learnqa.ru/api/homework_cookie")
-                .andReturn();
-        Map<String, String>    expected = saveExpected.getCookies();
-        Set<String> setKeys = expected.keySet();
+        Map<String, String> expected = new HashMap<>();
+        expected.put("HomeWork", "hw_value");
+        String key = expected.keySet().iterator().next();
 
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/homework_cookie")
                 .andReturn();
 
-        for (String k : setKeys) {
-            assertEquals(expected.get(k), response.getCookie(k), "Unexpected cookie");
-        }
+        assertTrue(response.getCookies().containsKey(key));
+        assertEquals(expected.get(key), response.getCookie(key));
+
+
     }
 }
