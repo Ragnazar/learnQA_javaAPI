7c4ad81ea528fb358275fe2241165f07044f16b7
Ivan Avdeyev <97154352+Ragnazar@users.noreply.github.com>
2023-07-05 15:10:00 +0300

    add assertions: responseCode, textEquals

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -7,9 +7,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Assertions {
     public static void assertJsonByName(Response Response, String name, int expectedValue) {
-        Response.then().assertThat().body("$", hasKey(name));
+        Response
+                .then()
+                .assertThat()
+                .body("$", hasKey(name));
 
         int value = Response.jsonPath().getInt(name);
         assertEquals(expectedValue, value, "JSON value is not equal to expected value");
+    }
+
+    public static void assertResponseTextEquals(Response Response, String expectedAnswer){
+        assertEquals(
+                expectedAnswer,
+                Response.asString(),
+                "Response text is not as expected"
+        );
+    }
+    public static void assertResponseCodeEquals(Response Response, int expectedCode){
+        assertEquals(
+                expectedCode,
+                Response.getStatusCode(),
+                "Response code is not as expected"
+        );
     }
 }
